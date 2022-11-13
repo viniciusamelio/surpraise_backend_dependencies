113ffec603a048daff9987f616e0d7415efcb977
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-11-13 11:58:25 -0300

    exporting dev dependencies also

--- lib/src/packages.dart
+++ lib/src/packages.dart
@@ -1,4 +1,8 @@
+// ignore_for_file: depend_on_referenced_packages
+
 export 'package:equatable/equatable.dart';
 export 'package:string_validator/string_validator.dart';
 export 'package:ez_either/ez_either.dart';
 export 'package:mongo_dart/mongo_dart.dart';
+export 'package:mocktail/mocktail.dart';
+export 'package:faker/faker.dart';
