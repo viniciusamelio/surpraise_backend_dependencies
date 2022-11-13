cc869535382654b1e999e1628e2f668c40fb7613
Vinicius Amélio <vinicius.amelio@zoho.com>
2022-11-13 12:02:25 -0300

    fix: removing dev dependencies exporting

--- lib/src/packages.dart
+++ lib/src/packages.dart
@@ -4,5 +4,3 @@ export 'package:equatable/equatable.dart';
 export 'package:string_validator/string_validator.dart';
 export 'package:ez_either/ez_either.dart';
 export 'package:mongo_dart/mongo_dart.dart';
-export 'package:mocktail/mocktail.dart';
-export 'package:faker/faker.dart';
